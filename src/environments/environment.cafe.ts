9b7e3117466df8090f2bbb0048a97b164017aba4
franztutator <franz.yapu@tutatordev.com>
2025-11-17 15:10:45 -0400

    Fix user-log

--- src/environments/environment.cafe.ts
+++ src/environments/environment.cafe.ts
@@ -3,7 +3,7 @@ import { environmentDefault } from './default';
 export const environment = {
   ...environmentDefault,
   production: true,
-  backend: 'https://cafe.vertexhost.cloud/api/',
-  Socket: 'https://cafe.vertexhost.cloud',
+  backend: 'https://vertexhost.cloud/api/',
+  Socket: 'https://vertexhost.cloud',
   jwtKey: 'jwtToken'
 };
