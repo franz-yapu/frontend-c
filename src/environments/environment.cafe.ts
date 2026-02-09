c26186bf42a46bbe7453eec181749a01dddc6ab3
franztutator <franz.yapu@tutatordev.com>
2026-02-08 21:29:57 -0400

    fix url

--- src/environments/environment.cafe.ts
+++ src/environments/environment.cafe.ts
@@ -3,7 +3,7 @@ import { environmentDefault } from './default';
 export const environment = {
   ...environmentDefault,
   production: true,
-  backend: 'https://caritas.vertexhost.cloud/api/',
+  backend: 'https://caritas.vertexhost.cloud/api',
   Socket: 'https://caritas.vertexhost.cloud',
   jwtKey: 'jwtToken'
 };
